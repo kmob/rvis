bd309a95e2a02402bd4d4850162995060a05d895
Kevin O'Brien <kevin@obrien-consulting.com>
2020-06-02 09:48:50 -0500

    echarts added but
    not working need some way to used dom GetElementByID

--- src/main/rvis/main.cljs
+++ src/main/rvis/main.cljs
@@ -1,7 +1,8 @@
 (ns rvis.main
 	(:require [reagent.core :as reagent]
 		  [reagent.dom :as reagent-dom]
-		  ["showdown" :as showdown]))
+		  ["showdown" :as showdown]
+		  ["echarts" :as echarts]))
 
 ;; install showdown using npm "npm install --save showdown"
 ;; which adds showdown to package.json
@@ -16,6 +17,10 @@
 ;;    text      = '# hello, markdown!',
 ;;    html      = converter.makeHtml(text);
 
+;; npm install --save echarts
+;; adds echarts to package.json
+;; require as ["echarts" :as echarts]
+
 (defonce markdown (reagent/atom ""))
 
 (defonce showdown-converter (showdown/Converter.))
@@ -23,13 +28,28 @@
 (defn md->html [md]
 	(.makeHtml showdown-converter md))
 
+(def pie_chart 
+	{:title {:text "Pie Chart"}
+	 :series [{:name "pie"
+		   :type "pie"
+		   :data [{:value 400 :name "A"}
+			  {:value 335 :name "B"}]}]}) 
+
+(defonce mychart (echarts/init.))
+
+(defn chart_1 [options]
+	(.setOption mychart options))
+
+
 (defn app [] 
   [:div 
 	[:h1 "Hello!"]
 	[:textarea
 	 {:on-change #(reset! markdown (-> % .-target .-value))
 	  :value @markdown}]
- 	[:div (md->html @markdown)]])
+ 	[:div (md->html @markdown)]
+	[:h2 "Graph"]
+	[:div (chart_1 pie_chart)]])
 
 (defn mount! []
 	(reagent-dom/render [app]
