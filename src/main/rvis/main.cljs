c81def03c293766569fa508c238802a5ec1d410b
Kevin O'Brien <kevin@obrien-consulting.com>
2020-06-22 13:36:46 -0500

    add alternative passing map to echart-inner

--- src/main/rvis/main.cljs
+++ src/main/rvis/main.cljs
@@ -2,7 +2,6 @@
   (:require [reagent.core :as reagent]
        [reagent.dom :as reagent-dom]
        ["showdown" :as showdown]
-       ["codemirror" :as codemirror]
        ["echarts" :as echarts]))
 
 ;; SHOWDOWN
@@ -29,11 +28,14 @@
 ;; https://echarts.apache.org/en/api.html#echartsInstance.setOption
 ;; simple echarts example
 ;; https://echarts.apache.org/en/tutorial.html#Get%20Started%20with%20ECharts%20in%205%20minutes
+
 ;; the atom to hold the options
 (defonce chart-options (reagent/atom ""))
+
 ;; create the chart component and lifecycle methods
 ;; including a function to .init the component
-;; and to run .setOption to load the chart options passed as props
+;; and to run .setOption to load the chart options passed as props from an atom
+;; ---------------------------------------
 (defn echart-inner []
   (reagent/create-class {:display-name "echart-component"
                          :component-did-mount (fn [comp]
@@ -41,20 +43,40 @@
                                                       options (clj->js (reagent/props comp))]
                                                   (.setOption chart options)))
                          :reagent-render (fn [comp] [:div  {:style {:width "600px" :height "300px"}}])}))
-;; Note: @config is a map so it gets passed as props and used with reagent/props in echart-inner.
+
+;; @config is a map so it gets passed as props and used with reagent/props in echart-inner.
 ;; Non-props values can be accessed via (reagent/argv comp)
 (defn echart-outer [config]
   [echart-inner @config])
+;; ---------------------------------------
+
+;; ALTERNATIVELY: the options can be passed as a map throuhg an argument to echart-inner
+;; which doesn't seem so magical as props
+;; ---------------------------------------
+; (defn echart-inner [chart-options]
+;   (reagent/create-class {:display-name "echart-component"
+;                          :component-did-mount (fn [comp]
+;                                                 (let [chart (echarts/init (reagent-dom/dom-node comp))
+;                                                       options (clj->js chart-options)]
+;                                                   (.setOption chart options)))
+;                          :reagent-render (fn [comp] [:div  {:style {:width "600px" :height "300px"}}])}))
+
+;; chart-options is a map here,
+;; so it gets passed as an argument required by echart-inner
+;; and doesn't need derefed
+; (defn echart-outer [config]
+;   [echart-inner config])
+;; ---------------------------------------
 
 ; chart options examples that should work
-(def pie_chart
+(def pie-chart
   {:title {:text "Pie Chart"}
    :series [{:name "pie"
              :type "pie"
              :data [{:value 400 :name "A"}
                     {:value 335 :name "B"}]}]})
 
-(def bar_chart {:title   {:text "Bar Chart"}
+(def bar-chart {:title   {:text "Bar Chart"}
                 :tooltip {}
                 :legend  {:data ["Sales"]}
                 :xAxis   {:data ["aik" "amidi" "chiffon shirt" "pants" "heels" "socks"]}
@@ -63,13 +85,13 @@
                            :type "bar"
                            :data [5, 20, 36, 10, 20, 30]}]})
 
-(def line_chart {:title {:text "Line Chart"}
+(def line-chart {:title {:text "Line Chart"}
                  :xAxis {:type "category" :data ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]}
                  :yAxis {:type "value"}
                  :series [{:type "line"
                            :data [820, 932, 901, 934, 1290, 1330, 1320]}]})
 
-(reset! chart-options pie_chart)
+(reset! chart-options pie-chart)
 
 (defn app []
   [:div
@@ -77,6 +99,7 @@
    [:div
     [:h2 "Build echarts"]
     [:div [echart-outer chart-options]]]
+    ; [:div [echart-outer pie-chart]]]
    [:div
     [:h2 "Markdown to HTML Converter"]
     [:div (pr-str @markdown)]
